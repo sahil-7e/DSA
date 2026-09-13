6859ea5c5dd7aca5a5a1f9b708ad8eda31cfa5b0
Sahil <125900372+sahil-7e@users.noreply.github.com>
2026-09-13 09:37:38 +0530

    Time: 6 ms (99.87%) | Memory: 47.5 MB (61.96%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minSteps(String s, String t) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
+        }
+
+        int steps = 0;
+
+        for (int count : freq) {
+            if (count > 0) {
+                steps += count;
+            }
+        }
+
+        return steps;
+    }
+}
